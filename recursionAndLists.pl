9b285a585fe0ab5ed91c1993cad9fbeb773dddb1
leon.schloemmer <leon.schloemmer@gmail.com>
2017-12-21 08:50:29 +0100

    Updated recursionAndLists.pl

--- recursionAndLists.pl
+++ recursionAndLists.pl
@@ -1,1 +1,13 @@
-prepend(E, [H, T]) :- ???
+head(Head, [Head| _]).
+tail(Tail, [_ |Tail]).
+
+prepend(NewElement, Tail, [NewElement | Tail]).
+
+addElement([], L, L).
+addElement([H|T], L, [H|ExtendedTail]) :- addElement(T, L, ExtendedTail).
+
+hasLength([], 0).
+hasLength([_ | Tail], L) :- hasLength(Tail, N), L is N+.
+
+remove(E, [E | T], T).
+remove(E, [Head | Tail], [Head | RemovedTail]) :- remove(E, Tail, RemovedTail).
